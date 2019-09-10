152203a8a151d966a68e96897e08ff4e310418ae
wally <wally@shipt.com>
2019-09-10 14:50:43 -0500

    add action to repo

---
+++ .github/actions/go-benchmark-action/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+cd "${GITHUB_WORKSPACE:-.}"
+
+set +e
+OUTPUT="$(benchcmp old.txt new.txt)"
+SUCCESS=$?
+echo $SUCCESS
+set -e
+
+# Post results back as comment.
+COMMENT="#### \`benchcmp\`
+\`\`\`
+$OUTPUT
+\`\`\`
+"
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+
+if [ "COMMENTS_URL" != null ]; then
+  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+fi
+
+exit $SUCCESS
