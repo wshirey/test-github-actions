050de5e92018ab7f4bada28925b65207f3edf283
wally <wally@shipt.com>
2019-09-11 11:16:00 -0500

    update entrypoint

--- .github/actions/go-benchmark-action/entrypoint.sh
+++ .github/actions/go-benchmark-action/entrypoint.sh
@@ -1,25 +1,36 @@
 #!/bin/sh
 set -e
 
+add_issue_comment() {
+   COMMENT="#### \`benchcmp \`
+    \`\`\`
+    $1
+    \`\`\`
+    "
+    PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+    COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+
+    if [ "COMMENTS_URL" != null ]; then
+        curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+    fi
+}
+
 run_go_benchmark() {
-    echo "checking out $GIT_COMMIT"
     GIT_COMMIT=$1
-    BENCHMARK_FILE=$HOME/$GIT_COMMIT.txt
+    BENCHMARK_FILE=$2
     git clean -ffdx && git reset --hard HEAD
     git checkout $GIT_COMMIT
-    echo "running project benchmarks"
     go test -run=NONE -benchmem=true -bench=. ./... > $BENCHMARK_FILE
-    echo "benchmark results:"
-    cat $BENCHMARK_FILE
-    return $BENCHMARK_FILE
 }
 
 main() {
-    OLD_BENCHMARK_FILE=$(run_go_benchmark $GITHUB_BASE_REF)
-    NEW_BENCHMARK_FILE=$(run_go_benchmark $GITHUB_HEAD_REF)
-    BENCHCMP_RESULTS=$(benchcmp $OLD_BENCHMARK_FILE $NEW_BENCHMARK_FILE)
-    echo "benchcmp results:"
-    echo $BENCHCMP_RESULTS
+    cd $GITHUB_WORKSPACE
+    run_go_benchmark $GITHUB_BASE_REF $HOME/$GITHUB_BASE_REF.txt
+    run_go_benchmark $GITHUB_HEAD_REF $HOME/$GITHUB_HEAD_REF.txt
+    echo "running benchcmp $HOME/$GITHUB_BASE_REF.txt $HOME/$GITHUB_HEAD_REF.txt"
+    BENCHCMP_RESULTS=$(benchcmp $HOME/$GITHUB_BASE_REF.txt $HOME/$GITHUB_HEAD_REF.txt)
+    add_issue_comment $BENCHCMP_RESULTS
 }
 
 main "$@"
+echo $?
